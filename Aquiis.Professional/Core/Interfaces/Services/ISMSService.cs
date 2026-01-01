8afe816460f29669e438a6f617beeee97fdb3592
CIS Guru <cisguru@outlook.com>
2026-01-01 06:46:24 -0600

    Added Aquiis.Professional project

---
+++ Aquiis.Professional/Core/Interfaces/Services/ISMSService.cs
@@ -0,0 +1,7 @@
+
+namespace Aquiis.Professional.Core.Interfaces.Services;
+public interface ISMSService
+{
+    Task SendSMSAsync(string phoneNumber, string message);
+    Task<bool> ValidatePhoneNumberAsync(string phoneNumber);
+}
